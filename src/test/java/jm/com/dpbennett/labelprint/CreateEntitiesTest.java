4b7e325a286ba2fbaecc779ffab2840da0875495
desbenn <desbenn@Aspire-ES1-512>
2020-07-11 15:35:01 -0500

    General updates.

---
+++ src/test/java/jm/com/dpbennett/labelprint/CreateEntitiesTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jm.com.dpbennett.labelprint;
+
+import java.util.HashMap;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import jm.com.dpbennett.business.entity.mt.EnergyLabel;
+import jm.com.dpbennett.business.entity.sc.Complaint;
+import org.junit.Test;
+
+/**
+ *
+ * @author Desmond Bennett <info@dpbennett.com.jm at http//dpbennett.com.jm>
+ */
+public class CreateEntitiesTest {
+    @Test
+    public void test() {
+        HashMap prop = new HashMap();
+
+        prop.put("javax.persistence.jdbc.user",
+                "root");
+        prop.put("javax.persistence.jdbc.password",
+                ""); // NB: REMOVE PWD WHEN DONE AND SET SKIPTEST TO TRUE
+        prop.put("javax.persistence.jdbc.url",
+                "jdbc:mysql://localhost:3306/enerbase");
+        //prop.put("javax.persistence.jdbc.driver",
+        //        "com.mysql.jdbc.Driver");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("LabelPrintPU", prop);
+        EntityManager em = emf.createEntityManager();
+
+       // These instantiations cause the respective database tables to be created
+       // if they don't already exist.
+       // NB: Set <skipTests>true</skipTests> when done.
+       EnergyLabel e = new EnergyLabel();
+
+    }
+}
